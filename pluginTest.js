2f7c2ab5050024760e06292e09716120bedc9179
Josep Abellana <josep.abe.pu@gmail.com>
2023-01-24 16:57:04 +0100

    x

---
+++ pluginTest.js
@@ -0,0 +1,12 @@
+(function () {
+  amp.plugin("telemetry", function (options) {
+    var player = this;
+
+    var init = function () {
+      console.log("plugin telemetry initialized with player ", player);
+    };
+
+    // initialize the plugin
+    init();
+  });
+}.call(this));
